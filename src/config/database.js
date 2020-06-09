c4505e73412d08e6dcdab2575ae3d947ec18515a
Renan Lucas <renanluquinhas53@gmail.com>
2020-06-09 20:07:14 -0300

    Database config

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'docker',
+    database: 'plathanus',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
